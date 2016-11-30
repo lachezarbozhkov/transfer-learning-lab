155ef37aa629a81a0d14c101ef85346969708f0f
Lachezar Bozhkov <lachezar.bozhkov@gmail.com>
2016-11-30 12:35:19 +0200

    Add download data script

---
+++ download_data.sh
@@ -0,0 +1,6 @@
+wget https://d17h27t6h515a5.cloudfront.net/topher/2016/November/5834b432_vgg-100/vgg-100.zip
+wget https://d17h27t6h515a5.cloudfront.net/topher/2016/November/5834b634_resnet-100/resnet-100.zip
+wget https://d17h27t6h515a5.cloudfront.net/topher/2016/November/5834b498_inception-100/inception-100.zip
+
+wget https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+tar -xzvf cifar-10-python.tar.gz
